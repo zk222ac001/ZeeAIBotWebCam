c7ce1f6c8345ef817d127226ec30b2075d08d87e
zk222ac001 <zk222ac@gmail.com>
2026-09-05 23:58:38 +0200

    Phase 11: register audio pipeline router

--- src/robotic_classroom/conference/__init__.py
+++ src/robotic_classroom/conference/__init__.py
@@ -1,1 +1,6 @@
 """WebRTC conference transport services."""
+
+from robotic_classroom.conference.audio_router import router as audio_router
+from robotic_classroom.conference.router import router as conference_router
+
+conference_router.include_router(audio_router)
